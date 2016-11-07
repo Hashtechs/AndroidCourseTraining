99518dfa755db50fe54d00a9a73977f704430a1f
muwaffaq imam <muwaffaqimam@gmail.com>
2016-11-07 22:27:23 +0200

    Initial commit

---
+++ app/src/main/java/umniahap/com/umniahapp1/activites/MainActivity.java
@@ -0,0 +1,15 @@
+package umniahap.com.umniahapp1.activites;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import umniahap.com.umniahapp1.R;
+
+public class MainActivity extends AppCompatActivity  {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_screen);
+    }
+}
